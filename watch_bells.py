e7a92954f3497268fb91ac9a52dc1e86dccaaece
Euan Reid <euan@euanreid.com>
2019-10-27 14:38:37 -0700

    Title text and mute toggle

--- watch_bells.py
+++ watch_bells.py
@@ -10,14 +10,36 @@ from playsound import playsound
 TWO_CHIMES = 'https://freesound.org/data/previews/353/353232_5477651-lq.mp3'
 ONE_CHIME = 'https://freesound.org/data/previews/353/353233_5477651-lq.mp3'
 
-running = True
+WATCH_TIMES = {
+    'first': range(2000, 2400),
+    'middle': range(0000, 400),
+    'morning': range(400, 800),
+    'forenoon': range(800, 1200),
+    'afternoon': range(1200, 1600),
+    'first_dog': range(1600, 1800),
+    'last_dog': range(1800, 2000)
+}
 
-icon = pystray.Icon(name='Watch Bells', title='Watch Bells')
+INT_TO_WORDS = {
+    1: 'One bell',
+    2: 'Two bells',
+    3: 'Three bells',
+    4: 'Four bells',
+    5: 'Five bells',
+    6: 'Six bells',
+    7: 'Seven bells',
+    8: 'Eight bells'
+}
+
+muted = False
+shutting_down = False
+
+icon = pystray.Icon(name='Watch Bells')
 
 
 def watch_clock(icon):
     icon.visible = True
-    while running:
+    while not shutting_down:
         next_bell = next_half_hour()
         seconds_to_wait = (
             (next_bell - datetime.datetime.now())
@@ -29,7 +51,11 @@ def watch_clock(icon):
             time.sleep(10)
         else:
             time.sleep(seconds_to_wait)
-            ring_bells_for_timestamp(next_bell)
+            watch = watch_for_datetime(next_bell)
+            bells = bells_for_datetime(next_bell)
+            icon.title = f'{INT_TO_WORDS[bells]} of the {watch} watch'
+            if not muted:
+                ring_bells(bells)
 
 
 def next_half_hour():
@@ -43,20 +69,24 @@ def next_half_hour():
     return dt
 
 
-def ring_bells_for_timestamp(dt):
+def watch_for_datetime(dt):
+    timestamp = int(f'{dt.hour}{dt.minute:02d}')
+    if timestamp not in range(0, 2400):
+        raise ValueError(f'Invalid timestamp {timestamp}')
+    for watch, timerange in WATCH_TIMES.items():
+        if timestamp in timerange:
+            return watch
+
+
+def bells_for_datetime(dt):
     bells = dt.hour % 4 * 2
     if dt.minute == 30:
         bells += 1
-    nore_adjustment_times = {
-        (18, 30),
-        (19, 00),
-        (19, 30)
-    }
-    if (dt.hour, dt.minute) in nore_adjustment_times:
-        bells -= 4
     if bells == 0:
         bells = 8
-    ring_bells(bells)
+    if watch_for_datetime(dt) == 'last_dog' and bells != 8:
+        bells -= 4
+    return bells
 
 
 def ring_bells(bells):
@@ -70,9 +100,14 @@ def ring_bells(bells):
         playsound(ONE_CHIME)
 
 
+def toggle_mute():
+    global muted
+    muted = not muted
+
+
 def quit_program():
-    global running
-    running = False
+    global shutting_down
+    shutting_down = True
     icon.stop()
 
 
@@ -92,6 +127,11 @@ def create_image(width, height):
 def create_menu():
     return pystray.Menu(
         pystray.MenuItem(
+            text='Mute',
+            action=toggle_mute,
+            checked=lambda item: muted
+        ),
+        pystray.MenuItem(
             text='Quit',
             action=quit_program
         )
@@ -102,4 +142,8 @@ icon.icon = create_image(100, 100)
 icon.menu = create_menu()
 
 if '__main__' == __name__:
+    dt = datetime.datetime.now()
+    watch = watch_for_datetime(dt)
+    bells = bells_for_datetime(dt)
+    icon.title = f'{INT_TO_WORDS[bells]} of the {watch} watch'
     icon.run(setup=watch_clock)
